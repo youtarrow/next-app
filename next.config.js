d7143850dafde40d5d00562218f1bd864cbf72b9
越中谷優 <y-ecchuya@gajumale.co.jp>
2021-02-07 16:54:30 +0900

    material-uiの実装

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  webpack(config) {
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
 };
